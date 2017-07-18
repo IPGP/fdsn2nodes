d082c16b1bd8e83c36c3b5a0e741e582a8124244
Jean-Marie Saurel <saurel@ipgp.fr>
2017-07-18 13:07:24 +0000

    Corrected CNF ACQ_RATE value which should be expressed in days.

--- fdsn2nodes.py
+++ fdsn2nodes.py
@@ -182,7 +182,7 @@ class NodesCreator(object):
             cnf_file.write("END_DATE|NA\n")
         else:
             cnf_file.write("END_DATE|%s\n" % (station.termination_date.strftime("%Y-%m-%d")))
-        cnf_file.write("ACQ_RATE|%s\n" % (str(sample_rate)))
+        cnf_file.write("ACQ_RATE|1/%s\n" % (str(86400*sample_rate)))
         cnf_file.write("UTC_DATA|+0\n")
         cnf_file.write("LAST_DELAY|1/24\n")
         cnf_file.write("FILES_FEATURES|sensor\n")
